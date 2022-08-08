5cefb63d8044849420e6dce22ac3a5a4202c3fa1
Harshal <harshaldharmik099@gmail.com>
2022-08-09 01:25:17 +0530

    Added error handlers and middlewares

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-
+import { errorHandler } from './middlewares/error-handler';
 const app = express();
 
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
-
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log('Auth Service listening on 3000');
 });
