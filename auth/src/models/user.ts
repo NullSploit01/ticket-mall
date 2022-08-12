2f4308ddf21ce42c4e13dc4bf3db59015925812f
Harshal <harshaldharmik099@gmail.com>
2022-08-13 01:56:08 +0530

    Added password hashing ability and some error handling

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import { Password } from '../services/password';
 // interface for describing required params to create new user
 
 interface UserAttrs {
@@ -25,6 +25,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
