d3ecf203d33657be78b8823cb729b5f1071fd6d4
Harshal <harshaldharmik099@gmail.com>
2022-08-30 00:43:23 +0530

    Added JWT Tokens and env secrets into cluster

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4-20 characters'),
+      .withMessage("Password must be between 4-20 characters"),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -22,11 +23,20 @@ router.post(
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      throw new BadRequestError('Email already in use');
+      throw new BadRequestError("Email already in use");
     }
     const user = User.build({ email, password });
     await user.save();
-
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+    req.session = {
+      jwt: userJwt,
+    };
     res.status(201).send(user);
   }
 );
