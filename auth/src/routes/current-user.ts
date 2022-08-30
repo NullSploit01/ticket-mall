fd327b29dcd46bd98a9e58fe43e271fc191ec079
Harshal <harshaldharmik099@gmail.com>
2022-08-31 01:16:09 +0530

    Add signin and signoutability:

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,16 @@
-import express from 'express';
-
+import express from "express";
+import jwt from "jsonwebtoken";
 const router = express.Router();
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('current-route');
+router.get("/api/users/currentuser", (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
